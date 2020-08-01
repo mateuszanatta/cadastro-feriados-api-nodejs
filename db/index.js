2e0c34db9aeb2149acbf7f6a91f073c2167dfaa6
Mateus da Rosa Zanatta <mateus_zanatta@hotmail.com>
2020-08-01 15:01:45 -0300

    alteracao db string

--- db/index.js
+++ db/index.js
@@ -1,10 +1,14 @@
 const { Pool } = require('pg')
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'feriados',
-  password: '123456',
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+  // user: 'postgres',
+  // host: 'localhost',
+  // database: 'feriados',
+  // password: '123456',
+  // port: 5432,
 })
 module.exports = {
   query: (text, params) => pool.query(text, params),
